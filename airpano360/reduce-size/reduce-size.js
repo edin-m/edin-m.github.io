ecf6cdd39dfc3d66e8fa624d39a3e3ef4ffcac1d
Edin Mujagic <edinmujagic2@gmail.com>
2025-05-10 09:30:17 +0200

    update airpano360

--- airpano360/reduce-size/reduce-size.js
+++ airpano360/reduce-size/reduce-size.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const inputFolder = '../originals'; // your input folder
-const outputFolder = '../pano'; // your output folder
+const outputFolder = '../panos'; // your output folder
 
 // Ensure output directory exists
 if (!fs.existsSync(outputFolder)) {
